ddaae5e88a0ada145cac653a8019a51496feadb3
ST10540680 <ST10540680@myemeris.edu.za>
2026-08-06 16:28:48 +0200

    updated the App.tsx

--- firstApp/App.tsx
+++ firstApp/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, Image, SafeAreaView, ScrollView } from 'react-native';
-import {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Image, SafeAreaView, ScrollView, Animated, ViewStyle, StyleProp} from 'react-native';
+import {Children, ReactNode, useEffect, useRef, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator, NativeStackScreenProps} from '@react-navigation/native-Stack;
 
@@ -25,7 +25,6 @@ type RootStackParamList = {
   >;
 
 export default function App() {
- 
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -37,7 +36,6 @@ export default function App() {
 }
 
 function Mainscreen({navigation}: MainscreenProps){
-
   const [Name, setName] = useState()
   const [Surname, setSurname] = useState('');
 
@@ -45,33 +43,45 @@ function Mainscreen({navigation}: MainscreenProps){
 
   return(
     <View>
-
     <SafeAreaView>
       <ScrollView>
-
       <Image style={styles.mainImg}
       source={require('./images/cupcake.jpg')}/>
 
       <Text style={styles.welcomeTxt}>welcome to my app!</Text>
-      
-     <View style={styles.InputFlex}>
-      <text style= {styles.headingTxt}>Enter your name:</text>
-      <TextInput style={styles.inputBoxTxt} 
-      placeholder = "Helder"
-      OnChangeText={newText => SetName(newText)}/>
 
-      <text style={styles.headingTxt}>Enter your surname:</text>
-      <TextInput style={styles. inputBoxTxt}
-      placeholder = "Filipe"
-      onChangeText={newText => setSurname(newText)}/>
-     </View> 
+      <FadeInView>
+        <Text style={styles.redTxt}>{Error}</Text>
+        <View style={styles.InputFlex}>
+          <Text style= {styles.headingTxt}>Enter your name:</Text>
+          <TextInput style={styles.inputBoxTxt} 
+          placeholder = "Helder"
+          OnChangeText={(text)=> SetName(text)}/>
+
+          <Text style={styles.headingTxt}>Enter your surname:</Text>
+          <TextInput style={styles. inputBoxTxt}
+          placeholder = "Filipe"
+          onChangeText={(text)=> setSurname(text)}/>
+        </View>
+     </FadeInView>
+     
 
       <Button title= "Add User"
-      onPress= {( => {
-        navigation.navigate('View', {
-          NameSend : Name,
-          SurnameSend : Surname 
-        });
+      onPress= {() => {
+
+        if (isEmpty(Name) || isEmpty(Surname) == false)
+        {
+          navigation.navigate('View', {
+            NameSend : Name,
+            SurnameSend : Surname 
+          });
+          setError('');
+        }
+        else{
+          setError('Fields are empty!');
+        }
+
+        console.log("Name:" + Name + "Surname: " + Surname);
       }}/>
 
       <StatusBar style="auto" />
@@ -79,48 +89,96 @@ function Mainscreen({navigation}: MainscreenProps){
       </SafeAreaView>
     </View>
   );
-};
+}
 
-function ViewDetails ({navigation, route }: ViewDetailsProps){
+function ViewDetails ({ navigation, route }: ViewDetailsProps){
   
   const NameGet = route.params.NameSend;
   const SurnameGet = route.params.surnameSend;
 
-  return(
+  return (
     <view style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Name: {NameGet} Surname: {SurnameGet}</Text>
+      <Text>Name: {NameGet}</Text>
+      <Text> Surname: {SurnameGet}</Text>
     </view>
   );
 };
+function isEmpty(value: any) {
+  return(
+    (value === null) ||
+    (value .hasOwnProperty('length') && value. length === 0) ||
+    (value. constructor === Object && Object. keys(value).length === 0)
+  )
+};
+
+interface FadeInViewProps{
+  style?: StyleProp<ViewStyle>;
+  children: ReactNode;
+  
+}
+
+const FadeInView = ({children, style}: FadeInViewProps) => {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() =>{
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1,
+        duration: 4000,
+        useNativeDriver: false
+      }
+    ).start();
+ },[fadeAnim])
+
+   return(
+    <Animated.View style = {{
+      ...(style as object),
+      opacity: fadeAnim
+    }}>
+      {children}
+
+    </Animated.View>
+  );
+};
+
 
 const styles = StyleSheet.create({
   welcomeTxt: {
     paddingTop: 50,
     color: "blue",
     fontWeight: 'bold',
-    fontSize: 30,
+    fontSize: 35,
     textAlign: 'center'
   },
 
   headingTxt: {
-    fontWeight: 'bold',
+    paddingTop: 20,
+    color: "Black",
+    fontWeight: '600',
+    fontSize: 18,
   },
 
   inputBoxTxt: {
-
+    borderWidth: 1,
   },
 
   mainImg: {
     paddingTop: 50,
     justifyContent: 'center',
     alignItems: 'center',
-    width: 250,
-    height: 250
+    width: 300,
+    height: 200
   },
 
   InputFlex:{
-    flexDirection:'row',
-    marginTop: 30,
+    marginTop: 20,
     justifyContent: 'space-evenly'
   },
+  redTxt: {
+    color: "red",
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center",
+  }
 });
